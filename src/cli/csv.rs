94c318891b56af8b135ee36b5d177213fe2ab5b2
xgh0627 <417644978@qq.com>
2024-04-21 16:32:59 +0800

    add csv basic code

--- src/cli/csv.rs
+++ src/cli/csv.rs
@@ -1,14 +1,45 @@
+use std::str::FromStr;
 use clap::Parser;
+use super::verify_file;
 
 #[derive(Debug,Parser)]
 pub struct CsvOpts {
-    #[arg(short,long)]
+    #[arg(short,long,value_parser = verify_file)]
     input: String,
 
-    #[arg(short,long,default_value = "output.json")]
-    output: String,
+    #[arg(long,value_parser = parse_format,default_value = "json")]
+    format: OutPutFormat,
 
+    #[arg(short,long)]
+    output: Option<String>,
+
+    #[arg(short,long,default_value_t = ',')]
     delimiter: char,
 
+    #[arg(short,long,default_value_t = true)]
     header: bool
+}
+
+#[derive(Debug,Clone,Copy)]
+enum OutPutFormat {
+    Json,
+    Yaml
+}
+
+fn parse_format(format: &str) -> Result<OutPutFormat,anyhow::Error> {
+    format.parse()
+}
+
+impl FromStr for OutPutFormat {
+    
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutPutFormat::Json),
+            "yaml" => Ok(OutPutFormat::Yaml),
+            _ => Err(anyhow::anyhow!("Invalid format"))
+        }
+    }
+
 }
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -16,3 +16,11 @@ enum SubCommand {
     #[command(name = "csv",about = "show csv or convert csv to other formats")]
     Csv(CsvOpts),
 }
+
+fn verify_file(filename: &str) -> Result<String,&'static str> {
+    if filename.len() == 0 || filename == "-" || std::path::Path::new(filename).exists() {
+        Ok(filename.into())
+    }else{
+        Err("file not found")
+    }
+}
